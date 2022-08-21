4d4f7d1dba23e25ed7c8c38066fae3d23a2e7639
Mateus Macêdo <mateusamacedo5@gmail.com>
2022-08-21 16:24:19 -0300

    Jogo finalizado e protótipo de cronometro

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 const lista = document.querySelector('.lista')
 const imgs = ['bobrossparrot','explodyparrot','fiestaparrot','revertitparrot','tripletsparrot','unicornparrot','metalparrot']
 const listaImg = []
+const certas = []
+let cronometro = document.querySelector('.cronometro')
+
+let tempo = 0
+let contador = 0
+
+function contaTempo(){
+    tempo++
+    cronometro.innerHTML = tempo+'s';
+}
+
+const abc = setInterval(contaTempo, 1000)
 
 imgs.sort(comparador)
 
@@ -19,7 +31,7 @@ function comparador() {
 }
 
 for (i=0; i<quantidade; i++) {
-    lista.innerHTML += `<li><div class="card ${listaImg[i]}" onclick="girar(this)">
+    lista.innerHTML += `<li><div class="card ${listaImg[i]} a${i}" onclick="girarVerificar(this)">
     <div class="frente face">
         <img src="media/front.png" alt="Front-parrot">
     </div>
@@ -27,8 +39,53 @@ for (i=0; i<quantidade; i++) {
     </div></li>`
 }
 
-function girar(elemento) {
-    const elemento0 = elemento.children[0]
-    const elemento1 = elemento.children[1]
+const verifica = []
+
+function desvirar(){
+    verifica[0].children[0].classList.remove('frenteVirado')
+    verifica[0].children[1].classList.remove('trasVirado')
+    verifica[1].children[0].classList.remove('frenteVirado')
+    verifica[1].children[1].classList.remove('trasVirado')
+    verifica.pop()
+    verifica.pop()
+    console.log('desvira')
+}
+
+function girar(a){
+    const elemento0 = a.children[0]
+    const elemento1 = a.children[1]
     elemento1.classList.add('trasVirado')
-    elemento0.classList.add('frenteVirado')}
+    elemento0.classList.add('frenteVirado')
+    verifica.push(a)
+    contador++
+    console.log('gira')
+}
+function vitoria(){
+    alert(`Você ganhou em ${contador} jogadas e em ${tempo}s!`)
+}
+
+function girarVerificar(elemento) {
+    if (verifica.length<2 && !elemento.classList.contains('certo') && !verifica.includes(elemento)){
+        console.log('entrei');
+        girar(elemento);
+    
+        if (verifica.length===2) {
+            console.log('aqui tb, kkkkk');
+            if (verifica[0].classList[1]===verifica[1].classList[1]){
+                verifica[0].classList.add('certo')
+                verifica[1].classList.add('certo')
+                certas.push(verifica[0])
+                certas.push(verifica[1])
+                verifica.pop()
+                verifica.pop()
+            } else if (verifica.length===2){
+                console.log('aqui');
+                setTimeout(desvirar, 1000);
+            }
+        }
+        if (certas.length===Number(quantidade)){
+            clearInterval(1)
+            setTimeout(vitoria, 500)
+        }
+    }
+}
